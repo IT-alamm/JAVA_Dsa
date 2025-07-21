7b7106fdc3441a20bc52fb299f437a393265c943
IT-alamm <ma9788181@gmail.com>
2025-07-21 11:34:44 +0530

    adding star problem

--- Pattern/star.java
+++ Pattern/star.java
@@ -3,7 +3,7 @@ package Pattern;
 public class star {
     public static void main(String[] args) {
         int n=10;
-        System.out.println("hello");
+        System.out.println("hellooo");
         for (int i = 1; i <=n; i++) {
             for (int j = 1; j <=i; j++) {
                 System.out.print("*");
