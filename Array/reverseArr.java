61ef5ffc0c2ede4227648dd18306e7ac844e9d8e
IT-alamm <ma9788181@gmail.com>
2025-07-23 22:18:06 +0530

    add revrese array

--- Array/reverseArr.java
+++ Array/reverseArr.java
@@ -1,5 +1,26 @@
 package Array;
 
 public class reverseArr {
-    
+    public static void reverseArr(int number[]){
+        int first = 0; int last = number.length-1;
+
+        while(first < last){
+            int temp = number[first];
+            number[first] = number[last];
+            number[last] = temp;
+            first++;
+            last--;
+           
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        int number[] = {2, 4, 5, 8, 9, 7};
+         reverseArr(number);
+         for(int i = 0; i<number.length; i++){
+            System.out.print(number[i] +"  ");
+         }
+        //  System.out.println();
+    }
 }
