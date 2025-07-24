4a0f374997d30d1ce5f63cf326eeb6b7a83415a4
IT-alamm <ma9788181@gmail.com>
2025-07-24 21:54:44 +0530

    adding pair in array

---
+++ Array/pair.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class pair {
+    public static void printPair(int number[]){
+        for(int i = 0; i<number.length; i++){
+           int curr = number[i];
+           for(int j = i+1; j<number.length; j++){
+            System.out.print("(" + curr + "," + number[j] + ")");
+            
+           }
+           System.out.println();
+        }
+        
+
+    }
+
+
+    public static void main(String[] args) {
+        int number[] = {2, 4, 6, 8, 10, 11};
+         printPair(number);
+    }
+}
