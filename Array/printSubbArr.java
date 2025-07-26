0b59adfc2ab871ebac92d68d4e132dbfd15220fa
IT-alamm <ma9788181@gmail.com>
2025-07-26 23:15:08 +0530

    adding print subb array

---
+++ Array/printSubbArr.java
@@ -0,0 +1,30 @@
+package Array;
+
+public class printSubbArr {
+    public static void PrintSubbArray(int number[]){
+        int  ts = 0;
+    
+        for(int i = 0; i<number.length; i++){
+            int start = i;
+            for(int j = i; j<number.length; j++){
+                int end = j;
+                for(int k= start; k<=end; k++){
+                    System.err.print(number[k] +"  ");
+                }
+                ts++;
+    
+                System.out.println();
+            }
+            System.out.println();
+    
+        }
+    
+        System.out.println("total sub arrar : "  + ts);
+       }
+    
+        public static void main(String[] args) {
+            int number[] = {2, 4, 6, 8, 10};
+            PrintSubbArray(number);
+    
+        }
+}
