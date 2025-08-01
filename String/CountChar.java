a7bbc0f354d577517ef77311afd44eff62c8cbaf
IT-alamm <ma9788181@gmail.com>
2025-08-01 22:06:59 +0530

    adding a string value to count a char

---
+++ String/CountChar.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class CountChar {
+    public static void main(String[] args) {
+       
+
+        // for user input values 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string is : ");
+        String alpha = sc.next();
+        int length = alpha.length();
+        System.out.println(length);
+
+        
+
+        // for defined values 
+        // String str = "java weqrhrhj484545457020424335446575#$%";
+        // int length = str.length();
+        // System.out.println("char is : "+str.charAt(5));
+        // System.out.println("count : " +length);
+       
+    }
+    
+}
