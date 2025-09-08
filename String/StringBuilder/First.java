a664601add7698a05a710eb7ed43ff1bd635bc27
IT-alamm <ma9788181@gmail.com>
2025-09-08 17:43:16 +0530

    adding some String Builder loigc

---
+++ String/StringBuilder/First.java
@@ -0,0 +1,13 @@
+
+public class First {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World").reverse();
+        System.out.println(sb);
+        String str = "Hello World";
+        System.out.println(str.indexOf("l"));
+        System.out.println(str.isEmpty());
+   
+    }
+    
+}
